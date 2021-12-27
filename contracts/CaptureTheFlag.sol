a5094a7d1a61c25d02d399505f553f99278e0384
Henri93 <henryrgarant@gmail.com>
2021-12-27 11:29:33 -0500

    polgygon changes

--- contracts/CaptureTheFlag.sol
+++ contracts/CaptureTheFlag.sol
@@ -7,5 +7,5 @@ import "./ERC721Tradable.sol";
 // The Flag that people can 'capture' by owning
 // only the owner can update the URI to point to their meta data
 contract CaptureTheFlag is ERC721Tradable  {
-    constructor(address _proxyRegistryAddress) ERC721Tradable("CaptureTheFlag", "FLAG", "https://gateway.pinata.cloud/ipfs/QmaqLCuKYRkf2ntHKxRqRx1d8nTiS85Xo21ygWzewawH5q", _proxyRegistryAddress) {}
+    constructor(address _proxyRegistryAddress) ERC721Tradable("CaptureTheFlag", "FLAG", "https://gateway.pinata.cloud/ipfs/QmNXuifoCMkYdPxcc9SsSW1xgwKCw25XGfaNHEguRcxQGW", _proxyRegistryAddress) {}
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -5,13 +5,23 @@ require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
 const { API_URL, PRIVATE_KEY } = process.env;
 module.exports = {
-   solidity: "0.8.0",
-   defaultNetwork: "ropsten",
+   solidity: {
+      version: "0.8.0",
+      settings: {
+      optimizer: true,
+    }
+   },
+   defaultNetwork: "mainnet",
    networks: {
       hardhat: {},
       ropsten: {
          url: API_URL,
          accounts: [`0x${PRIVATE_KEY}`]
-      }
+      },
+      mainnet: {
+            url: API_URL,
+            accounts: [`0x${PRIVATE_KEY}`],
+            gasPrice: 33000000000
+        }
    },
 }
